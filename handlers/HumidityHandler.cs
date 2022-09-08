2e55fb31f1a4cce9cb3d85b7c4ba44cdb60c4940
matheus <anyonebr1@gmail.com>
2022-09-08 14:44:25 -0300

    task: added humidity handlers

---
+++ handlers/HumidityHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+namespace HazeMonitoring.handlers;
+
+public class HumidityHandler
+{
+    public APIGatewayProxyResponse Index(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var response = new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Headers = new Dictionary<string, string>
+            {
+                {"Content-Type", "application/json"},
+                {"Access-Control-Allow-Origin", "*"}
+            }
+        };
+        return response;
+    }
+        
+    public APIGatewayProxyResponse Create(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var response = new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Headers = new Dictionary<string, string>
+            {
+                {"Content-Type", "application/json"},
+                {"Access-Control-Allow-Origin", "*"}
+            }
+        };
+        return response;
+    }
+}
