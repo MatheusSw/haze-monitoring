626712871a84663305bc4cf3da024b30dc5a56e3
matheus <anyonebr1@gmail.com>
2022-09-08 14:29:33 -0300

    task: initial working commit

---
+++ handlers/TemperatureHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(
+typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace HazeMonitoring.handlers
+{
+    public class TemperatureHandler
+    {
+        public APIGatewayProxyResponse Index(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string>
+                {
+                    {"Content-Type", "application/json"},
+                    {"Access-Control-Allow-Origin", "*"}
+                }
+            };
+            return response;
+        }
+        
+        public APIGatewayProxyResponse Create(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string>
+                {
+                    {"Content-Type", "application/json"},
+                    {"Access-Control-Allow-Origin", "*"}
+                }
+            };
+            return response;
+        }
+    }
+}
