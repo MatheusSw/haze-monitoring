8ab7b8a501773acffe18816a4a282dc82dcdd0ec
matheus <anyonebr1@gmail.com>
2022-09-27 19:25:33 -0300

    task: added type to measurements response

--- models/dynamodb/MeasurementDbModel.cs
+++ models/dynamodb/MeasurementDbModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.Json.Serialization;
 using Amazon.DynamoDBv2.DataModel;
 
@@ -6,17 +7,26 @@ namespace HazeMonitoring.models.dynamodb;
 
 public class MeasurementDbModel
 {
-    [DynamoDBHashKey("PK")]
-    [JsonPropertyName("id")]
-    public string HashKey { get; set; }
+    [DynamoDBHashKey("PK")] [JsonIgnore] public string HashKey { get; set; }
 
     [DynamoDBRangeKey("SK")] [JsonIgnore] public string SortKey { get; set; }
 
     [DynamoDBProperty("Measurement-reading")]
     [JsonPropertyName("reading")]
     public decimal Reading { get; set; }
-    
+
     [DynamoDBIgnore]
     [JsonPropertyName("timestamp")]
-    public DateTime Timestamp { get; set; }
+    public DateTime Timestamp
+    {
+        get
+        {
+            var success = DateTime.TryParse(SortKey.Split("#")[1], CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var timestamp);
+            return success ? timestamp : default;
+        }
+    }
+    
+    [DynamoDBIgnore]
+    [JsonPropertyName("type")]
+    public string Type => SortKey.Split("#")[0].Split("-")[1];
 }
